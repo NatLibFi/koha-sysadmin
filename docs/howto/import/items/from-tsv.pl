0680eb0b79f7ec971a7ea3ef151efa70af3646a0
NatLibFi Brunberg J <brunberg@users.noreply.github.com>
2021-03-15 16:51:40 +0200

    How Fennica microfilm items were imported
    Squashed commit of a temporary branch (additems):
    
    commit f451f984ca934691aa420c5c48266ae2b5b25b61
    Author: NatLibFi Brunberg J <brunberg@users.noreply.github.com>
    Date:   Mon Mar 15 16:44:41 2021 +0200
    
        Items successfully imported in production
    
    commit 1a55bed30dff86d09028e0bdd215346a60391feb
    Author: NatLibFi Brunberg J <brunberg@users.noreply.github.com>
    Date:   Thu Mar 11 14:32:14 2021 +0200
    
        itemnotes in subfield z are public
    
    commit c186602f2172a8e82688e4bcf0145447c67cb350
    Author: NatLibFi Brunberg J <brunberg@users.noreply.github.com>
    Date:   Thu Mar 11 11:44:15 2021 +0200
    
        Feedback fixes
    
    commit efe21ff7a164d58a329c5a65ee3a3cb8f1482f9d
    Author: NatLibFi Brunberg J <brunberg@users.noreply.github.com>
    Date:   Thu Mar 11 10:12:52 2021 +0200
    
        bugfix
    
    commit 329ca4490bf4440ee454c14766d717d19654f705
    Author: NatLibFi Brunberg J <brunberg@users.noreply.github.com>
    Date:   Wed Mar 10 17:13:23 2021 +0200
    
        Reindex separately after adding/importing items...
    
    commit d1a3fb48422056aaf22e664d12ccbe92abd0cdc4
    Author: NatLibFi Brunberg J <brunberg@users.noreply.github.com>
    Date:   Fri Mar 5 14:40:25 2021 +0200
    
        Test howto add items (for Fennica microfilms)

---
+++ docs/howto/import/items/from-tsv.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+sub timestamp { use POSIX; strftime('%Y%m%dT%H%M%SZ', gmtime) }
+
+# Get inside your FOSS ILS Koha Perl environment and uncomment!
+#use Koha::Item;
+#use Koha::SearchEngine::Indexer;
+
+# For efficiency, we will be doing separately (set to 0 to skip):
+my $do_import_items = 1;          # (one by one, when looping over <>)
+my $do_update_biblios_index = 1;  # (for biblionumbers encountered)
+
+my %to_reindex;
+
+# Define some default subfield values for all items to be added
+# ( https://wiki.koha-community.org/wiki/Holdings_data_fields_(9xx) )
+my (    $_8,    $a,     $b,     $c,     $o,     $y ) =
+    qw( FEN     MFVAR   MFVAR   VARMF   MF      LS );
+
+# USAGE: ./from-tsv.pl [ITEMSFILE]...
+# (OR use standard input instead of named files)
+while (my $itsv = <>) {
+    chomp $itsv;
+
+    # We should have read one line from our input,
+    # with these tab-separated item subfield values:
+    my ($biblionumber, $k, $h, $z) = split /\t/, $itsv;
+    say "Skipping\t$itsv" and next  # Maybe an empty line?
+        unless defined $biblionumber && $biblionumber =~ /^\d+$/;
+
+    if ($do_import_items) {
+        my $dontindex = { skip_record_index => 1 };
+        # Log what we are processing to standard output
+        print timestamp, " adding\t$itsv\t";
+        # If this fails, you kept log, didn’t you?
+        my $item = Koha::Item->new( {
+                biblionumber => $biblionumber,  # Die if nonexisting.
+                holding_id => $k,  # Does your Koha have MFHD records?
+                ccode => $_8,
+                homebranch => $a,
+                holdingbranch => $b,
+                location => $c,
+                enumchron => $h,
+                itemcallnumber => $o,
+                #barcode => $p,
+                itype => $y,
+                itemnotes => $z,
+            } )->store($dontindex);
+        say my $inum = $item->itemnumber();  # Finish successful logline
+        $item->set({ barcode => "TekoMFinum$inum" })->store($dontindex);
+    }
+    ++$to_reindex{$biblionumber};
+}
+
+if ($do_update_biblios_index) {
+    my $count = my @bnos = sort keys %to_reindex;
+    say timestamp, " updating search index for $count biblionumbers";
+    Koha::SearchEngine::Indexer->new(
+        { index => $Koha::SearchEngine::BIBLIOS_INDEX }
+    )->index_records(\@bnos, 'specialUpdate', 'biblioserver');
+}
+
+say timestamp, " finishing.";
